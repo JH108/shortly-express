86cab0e8ebf0cae6bcfeb9b82926dfbb935304bb
Jesse Hill <jsnahill@gmail.com>
2017-03-06 21:05:16 -0800

    First days work
    Create users table and users model

--- app/models/user.js
+++ app/models/user.js
@@ -2,9 +2,16 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-
-
 var User = db.Model.extend({
+  tableName: 'users',
+  initialize: function(username, password) {
+    this.set('username', username);
+    this.set('password', bcrypt.hashSync(password));
+  },
+
+  validate: function(password) {
+    bcrypt.compareSync(password, this.get('password'));
+  }
 });
 
 module.exports = User;
